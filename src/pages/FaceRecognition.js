7a40bfe24b277f3bb3ade81d754f59281773c44f
ifauzan9 <ifauzan9@gmail.com>
2024-03-02 20:44:42 +0700

    third commit

--- src/pages/FaceRecognition.js
+++ src/pages/FaceRecognition.js
@@ -89,7 +89,7 @@ export default function FaceRecognition() {
 
   useEffect(() => {
     loadModels();
-  });
+  }, []);
 
   const videoPlay = async () => {
     const labeledFaceDescriptors = await getLabeledFaceDescriptions();
